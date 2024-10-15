63c828f653722acdce8525a42c82bb1297c0f1dd
Francisco Menezes <franciscomenezesbh@gmail.com>
2024-10-15 19:15:14 -0300

    eslint-plugin-simple-import-sort

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react'
+import React from 'react'
 import { createRoot } from 'react-dom/client'
 import { App } from './App'
 
 import './styles/global.css'
 
+
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+  <React.StrictMode>
     <App />
-  </StrictMode>,
+  </React.StrictMode>,
 )
