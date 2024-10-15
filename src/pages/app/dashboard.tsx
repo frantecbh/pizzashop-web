93edba7bc0a6efa29a32fd57d5290ab6465cbff2
Francisco Menezes <franciscomenezesbh@gmail.com>
2024-10-15 19:43:37 -0300

    SEO pages

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export const Dashboard = () => {
   return (
+    <>
+    <Helmet title='Dashboard' />
     <div>dashboard</div>
+    </>
+    
   )
 }
--- src/pages/auth/sing-in.tsx
+++ src/pages/auth/sing-in.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import { Helmet } from 'react-helmet-async'
 
 export const SingIn = () => {
   return (
+    <>
+    <Helmet title='Login' />
     <div>SingInsing-in</div>
+    </>
+    
   )
 }
