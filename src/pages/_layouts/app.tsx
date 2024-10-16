934cc35748d7838732560c08c47c76ebfac41e33
Francisco Menezes <franciscomenezesbh@gmail.com>
2024-10-16 16:50:37 -0300

    layout app + header

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -1,11 +1,12 @@
 
+import { Header } from '@/components/header'
 import { Outlet } from 'react-router-dom'
 
 export const AppLayout = () => {
   return (
-    <div>
-      <h1>Cabeçalho</h1>
-      <div>
+    <div className='flex min-h-screen flex-col'>
+    <Header />
+      <div className='flex-1 flex flex-col gap-4 p-8 pt-6'>
         <Outlet />
       </div>
     </div>
